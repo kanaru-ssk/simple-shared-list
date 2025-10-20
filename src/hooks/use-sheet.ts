d13ef64324b6a63fae9f3c97786146a85e9722b5
kanaru-ssk <kanaru.ssk@gmail.com>
2025-10-21 00:23:01 +0900

    fix use-sheets

--- src/hooks/use-sheet.ts
+++ src/hooks/use-sheet.ts
@@ -14,22 +14,18 @@ export function useSheet() {
     return parsed.data;
   });
 
-  const getSheet = useCallback((spreadsheetId: string, sheetName: string) => {
-    const row = localStorage.getItem(LOCALSTORAGE_KEY.SHEETS);
-    if (!row) return;
-
-    const result = sheetsSchema.safeParse(JSON.parse(row));
-    if (!result.success) throw new Error(result.error.message);
-
-    return result.data.find(
-      (v) => v.spreadsheetId === spreadsheetId && v.sheetName === sheetName,
-    );
-  }, []);
+  const getSheet = useCallback(
+    (spreadsheetId: string, sheetName: string) => {
+      return sheets.find(
+        (v) => v.spreadsheetId === spreadsheetId && v.sheetName === sheetName,
+      );
+    },
+    [sheets],
+  );
 
   const addSheet = useCallback(
     (sheet: Omit<Sheet, "id">): Sheet => {
-      const id = crypto.randomUUID();
-      const newSheet = { id, ...sheet };
+      const newSheet = { id: crypto.randomUUID(), ...sheet };
       const newValue = [...sheets, newSheet];
       setSheets(newValue);
       localStorage.setItem(LOCALSTORAGE_KEY.SHEETS, JSON.stringify(newValue));
@@ -38,17 +34,23 @@ export function useSheet() {
     [sheets],
   );
 
-  function editSheet(sheet: Sheet) {
-    const newValue = [...sheets.map((v) => (v.id === sheet.id ? sheet : v))];
-    setSheets(newValue);
-    localStorage.setItem(LOCALSTORAGE_KEY.SHEETS, JSON.stringify(newValue));
-  }
+  const editSheet = useCallback(
+    (sheet: Sheet) => {
+      const newValue = [...sheets.map((v) => (v.id === sheet.id ? sheet : v))];
+      setSheets(newValue);
+      localStorage.setItem(LOCALSTORAGE_KEY.SHEETS, JSON.stringify(newValue));
+    },
+    [sheets],
+  );
 
-  function deleteSheet(id: string) {
-    const newValue = sheets.filter((v) => v.id !== id);
-    setSheets(newValue);
-    localStorage.setItem(LOCALSTORAGE_KEY.SHEETS, JSON.stringify(newValue));
-  }
+  const deleteSheet = useCallback(
+    (id: string) => {
+      const newValue = sheets.filter((v) => v.id !== id);
+      setSheets(newValue);
+      localStorage.setItem(LOCALSTORAGE_KEY.SHEETS, JSON.stringify(newValue));
+    },
+    [sheets],
+  );
 
   return { sheets, getSheet, addSheet, editSheet, deleteSheet };
 }
