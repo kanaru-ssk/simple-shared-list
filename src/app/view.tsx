0ff3d7a1c61b323ba4a3f723046315771e27f610
kanaru-ssk <kanaru.ssk@gmail.com>
2025-10-23 10:17:29 +0900

    add cta button

--- src/app/view.tsx
+++ src/app/view.tsx
@@ -1,9 +1,20 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 export function View() {
   return (
     <div>
       <header className="px-5 h-16 flex justify-between items-center">
         <h1 className="font-bold">Simple Shared List</h1>
       </header>
+
+      <main className="max-w-3xl mx-auto p-5">
+        <section className="text-center">
+          <Button size="lg" asChild>
+            <Link href="/sheets">Get Start for Free</Link>
+          </Button>
+        </section>
+      </main>
     </div>
   );
 }
