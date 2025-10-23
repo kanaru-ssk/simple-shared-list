8504390f84d7057022f3ea7e3857f2dd64f14a2d
kanaru-ssk <kanaru.ssk@gmail.com>
2025-10-23 10:15:07 +0900

    add header

--- src/app/view.tsx
+++ src/app/view.tsx
@@ -1,3 +1,9 @@
 export function View() {
-  return <div>view</div>;
+  return (
+    <div>
+      <header className="px-5 h-16 flex justify-between items-center">
+        <h1 className="font-bold">Simple Shared List</h1>
+      </header>
+    </div>
+  );
 }
